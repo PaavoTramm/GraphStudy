ee57738203b87ca648b1e172b9ccbc59c8f28de2
Paavo Tramm <paavo.tramm@gmail.com>
2022-01-07 14:30:37 +0200

    Work in progress - graph adjaceny matrix representatiion

---
+++ Models/AlgorithmDijkstraMatrix.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphStudy.Models
+{
+    public class AlgorithmDijkstraMatrix : Algorithm
+    {
+
+        public AlgorithmDijkstraMatrix(State state)
+            : base(state)
+        {
+
+        }
+
+        Matrix? m_matrix;
+
+        public override bool Prepare(IEnumerable<Node> nodes)
+        {
+            if (m_state.Start == null)
+                m_state.Start = GetRandom(nodes);
+
+            if(m_state.End != null)
+            {
+                if(object.ReferenceEquals(m_state.End, m_state.Start))
+                    m_state.End = null;
+            }
+
+            return m_state.Start != null;
+        }
+
+        public override bool Execute(IEnumerable<Node> nodes)
+        {
+            Clear();
+
+            Prepare(nodes);
+
+            if (m_state.Start == null)
+                return false;
+
+            m_matrix = new Matrix(nodes);
+
+            int src = m_matrix[m_state.Start];
+            int dst = m_matrix[m_state.End]; // -1 when end == null
+
+            double[] dist = new double[m_matrix.N];
+            bool[] visited = new bool[m_matrix.N];
+
+            for (int i = 0; i < m_matrix.N; i++)
+            {
+                dist[i] = Infinity;
+                visited[i] = false;
+            }
+
+            dist[src] = 0;
+
+            bool done = false;
+
+            for (int count = 0; !done && count < (m_matrix.N - 1); count++)
+            {
+                int u = minDistance(dist, visited);
+
+                visited[u] = true;
+
+                for (int v = 0; v < m_matrix.N; v++)
+                    if (!visited[v] && m_matrix[u, v] != 0 && dist[u] != Infinity && dist[u] + m_matrix[u, v] < dist[v])
+                    {
+                        dist[v] = dist[u] + m_matrix[u, v];
+                        if (dst >= 0 && v == dst)
+                        {
+                            done = true;
+                            break;
+                        }
+
+                    }
+            }
+
+            int n = 0;
+            foreach (Node node in nodes)
+            {
+                if (dist[n] != Infinity)
+                    m_state.Select(node);
+                n++;
+            }
+
+            return true;
+        }
+
+        void Clear()
+        {
+            m_matrix = null;
+        }
+
+        int minDistance(double[] dist, bool[] visited)
+        {
+            // Initialize min value
+            double min = Infinity;
+            int min_index = -1;
+
+            if (m_matrix != null)
+            {
+                for (int v = 0; v < m_matrix.N; v++)
+                    if (visited[v] == false && dist[v] <= min)
+                    {
+                        min = dist[v];
+                        min_index = v;
+                    }
+            }
+            return min_index;
+        }
+
+        static double Infinity
+        {
+            get => Double.PositiveInfinity;
+        }
+    }
+}
--- Models/Matrix.cs
+++ Models/Matrix.cs
@@ -8,6 +8,52 @@ namespace GraphStudy.Models
 {
     public class Matrix
     {
+        public Matrix(IEnumerable<Node> nodes)
+        {
+            // build index to speed up lookup
 
+            m_index = new Dictionary<Node, int>();
+            int i = 0;
+            foreach (Node node in nodes)
+                m_index[node] = i++;
+
+            // build NxN matrix of node-to-node costs
+
+            m_N = nodes.Count();
+            m_graph = new double[m_N, m_N];
+
+            int n = 0;
+
+            foreach (Node node in nodes)
+            {
+                m_graph[n, n] = 0;
+
+                foreach (Edge edge in node.Edges)
+                {
+                    m_graph[n, m_index[edge.Node]] = edge.Cost;
+                }
+                n++;
+            }
+        }
+
+        public int N 
+        { 
+            get => m_N; 
+        }
+
+        public int this[Node? n]
+        {
+            get { if(n == null || m_index == null) return - 1; return m_index[n]; }
+            set { if (n != null && m_index != null) m_index[n] = value; }
+        }
+
+        public double this[int r, int c]
+        {
+            get { return m_graph[r, c]; } 
+        }
+
+        int m_N = 0;
+        double[,] m_graph;
+        Dictionary<Node, int> m_index;
     }
 }
